e8a5b3949d14283e65de4f2d7aa5a65eff4084c6
Nariman <narimansayilov@gmail.com>
2024-07-17 14:46:28 +0400

    add new exception

---
+++ src/main/java/com/icare/model/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.icare.model.exception;
+
+public class AlreadyExistsException extends RuntimeException{
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
