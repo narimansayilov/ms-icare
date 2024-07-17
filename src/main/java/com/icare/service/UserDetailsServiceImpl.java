49fab5be9e2c1c192e6f8dd72625cd1edec79e4b
Nariman <narimansayilov@gmail.com>
2024-07-17 14:49:34 +0400

    add user detail service

---
+++ src/main/java/com/icare/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.icare.service;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import com.icare.dao.entity.UserEntity;
+import org.springframework.stereotype.Service;
+import com.icare.dao.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("USER_NOT_FOUND"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+    }
+}
