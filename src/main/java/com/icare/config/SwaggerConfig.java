3a91a6d84032ebf5f37edc9af30c5da171688704
Nariman <narimansayilov@gmail.com>
2024-07-17 14:49:03 +0400

    add swagger configuration

---
+++ src/main/java/com/icare/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.icare.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .info(new Info()
+                                .title("APPLICATION")
+                                .description("API for customers to rent items")
+                                .version("1.0.0")
+                                .contact(new Contact()
+                                        .name("Nariman Sayilov")
+                                        .email("narimansayilov@gmail.com")
+                                        .url("https://github.com/narimansayilov")))
+                        .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecuritySchema"))
+                        .components(new Components().addSecuritySchemes("JavaInUseSecuritySchema", new SecurityScheme()
+                                .name("JavaInUseSecuritySchema").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+        }
+}
