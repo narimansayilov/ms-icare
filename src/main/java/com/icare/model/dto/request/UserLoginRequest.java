fe3ab413d8a11e0b7b0acd5cb102acb4fa79229a
Nariman <narimansayilov@gmail.com>
2024-07-17 14:49:44 +0400

    add user login request

---
+++ src/main/java/com/icare/model/dto/request/UserLoginRequest.java
@@ -0,0 +1,28 @@
+package com.icare.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginRequest {
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+
+    @Size(min = 8, max = 64, message = "Password must be at least 8 characters long")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$",
+            message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character"
+    )
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
