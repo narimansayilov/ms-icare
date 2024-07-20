7f3616666bf2534c7654e9b9b4d99dd3393e57f2
Nariman <narimansayilov@gmail.com>
2024-07-20 23:54:51 +0400

    add new repository

--- src/main/java/com/icare/dao/repository/CityRepository.java
+++ src/main/java/com/icare/dao/repository/CityRepository.java
@@ -4,6 +4,10 @@ import com.icare.dao.entity.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    boolean existsByName(String name);
+    Optional<CityEntity> findByName(String name);
 }
