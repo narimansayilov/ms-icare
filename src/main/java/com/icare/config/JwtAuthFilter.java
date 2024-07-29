5fa3ce4b37c8fd0c0d610707c5aff7258a72c655
Nariman <narimansayilov@gmail.com>
2024-07-29 21:59:26 +0400

    change jwt auth filter

--- src/main/java/com/icare/config/JwtAuthFilter.java
+++ src/main/java/com/icare/config/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.icare.config;
 
+import com.icare.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
