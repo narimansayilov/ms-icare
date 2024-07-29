fea35f71d0405c352528e3fa641c229ae98535c1
Nariman <narimansayilov@gmail.com>
2024-07-29 22:01:19 +0400

    change entity structure

---
+++ src/main/java/com/icare/dao/entity/LevelEntity.java
@@ -0,0 +1,32 @@
+package com.icare.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "levels")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LevelEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Integer adLimit;
+    Double price;
+    boolean status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
