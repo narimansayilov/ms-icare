c8a1d7551996b98ed24466b1eff7db949842435d
Nariman <narimansayilov@gmail.com>
2024-07-29 21:58:06 +0400

    add new repository

--- src/main/java/com/icare/dao/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icare.dao.repository;
-
-import com.icare.dao.entity.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-}
--- src/main/java/com/icare/dao/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icare.dao.repository;
-
-import com.icare.dao.entity.StockEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<StockEntity, Long> {
-}
