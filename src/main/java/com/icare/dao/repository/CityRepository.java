35c83b1019b70326f1493399dbf310d921f9d3da
Nariman <narimansayilov@gmail.com>
2024-07-29 22:48:07 +0400

    add new repository

--- src/main/java/com/icare/dao/repository/CityRepository.java
+++ src/main/java/com/icare/dao/repository/CityRepository.java
@@ -4,8 +4,6 @@ import com.icare.dao.entity.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
 }
--- src/main/java/com/icare/dao/repository/UserRepository.java
+++ src/main/java/com/icare/dao/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.icare.dao.repository;
 
 import com.icare.dao.entity.UserEntity;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
