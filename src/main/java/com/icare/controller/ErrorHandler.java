0c5f2347844607379d42387c781011ffece753e3
Nariman <narimansayilov@gmail.com>
2024-07-30 16:33:31 +0400

    add new handler

--- src/main/java/com/icare/controller/ErrorHandler.java
+++ src/main/java/com/icare/controller/ErrorHandler.java
@@ -1,17 +1,13 @@
 package com.icare.controller;
 
 import com.icare.model.dto.response.ExceptionResponse;
+import com.icare.model.dto.response.FieldErrorResponse;
 import com.icare.model.exception.AlreadyExistsException;
 import com.icare.model.exception.NotFoundException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -21,7 +17,7 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
-public class ErrorHandler extends ResponseEntityExceptionHandler {
+public class ErrorHandler {
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MMMM-dd HH:mm:ss");
 
     @ExceptionHandler(NotFoundException.class)
@@ -46,6 +42,28 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ExceptionResponse handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        List<FieldErrorResponse> fieldErrors = new ArrayList<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            FieldErrorResponse error = FieldErrorResponse.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+            fieldErrors.add(error);
+        });
+
+        return ExceptionResponse.builder()
+                .timestamp(LocalDateTime.now().format(DATE_FORMATTER))
+                .message("VALIDATION_FAILED")
+                .code("INVALID_ARGUMENT")
+                .status(BAD_REQUEST.value())
+                .fieldErrors(fieldErrors)
+                .build();
+
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ExceptionResponse handleException(Exception exception) {
@@ -55,22 +73,5 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
                 .code("UNEXPECTED_EXCEPTION")
                 .status(INTERNAL_SERVER_ERROR.value())
                 .build();
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request){
-        List<ExceptionResponse> errors = new ArrayList<>();
-        exception.getBindingResult()
-                .getFieldErrors()
-                .forEach(e -> errors.add(ExceptionResponse.builder()
-                                .timestamp(LocalDateTime.now().format(DATE_FORMATTER))
-                                .message(exception.getMessage())
-                                .code("INVALID_ARGUMENT")
-                                .status(BAD_REQUEST.value())
-                        .build()));
-        return ResponseEntity.status(status).body(errors);
     }
 }
