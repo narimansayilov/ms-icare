bb1f9321836a1c5ffbc6127cc0ef50d9869dd936
Nariman <narimansayilov@gmail.com>
2024-07-30 17:51:56 +0400

    change security config

--- src/main/java/com/icare/config/SecurityConfig.java
+++ src/main/java/com/icare/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/users/login",
             "/users/register",
-            "categories/**",
+            "/categories/**",
+            "/cities/**",
             "/webjars/**",
             "/v2/api3-docs",
             "/v3/api-docs/**",
