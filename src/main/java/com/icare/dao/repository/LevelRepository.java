b5ec5eb26a0d7eed7a61cc5983f5371642044a2f
Nariman <narimansayilov@gmail.com>
2024-08-01 18:47:00 +0400

    change level repository

--- src/main/java/com/icare/dao/repository/LevelRepository.java
+++ src/main/java/com/icare/dao/repository/LevelRepository.java
@@ -4,6 +4,9 @@ import com.icare.dao.entity.LevelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LevelRepository extends JpaRepository<LevelEntity, Long> {
+    Optional<LevelEntity> findByName(String name);
 }
