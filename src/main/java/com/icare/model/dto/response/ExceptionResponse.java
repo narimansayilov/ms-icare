af755d667db6aa4bd93236ca6e3337e281d26632
Nariman <narimansayilov@gmail.com>
2024-08-05 20:09:54 +0400

    change exception response

--- src/main/java/com/icare/model/dto/response/ExceptionResponse.java
+++ src/main/java/com/icare/model/dto/response/ExceptionResponse.java
@@ -4,8 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @Builder
@@ -14,5 +12,4 @@ public class ExceptionResponse {
     private String message;
     private String code;
     private Integer status;
-    private List<FieldErrorResponse> fieldErrors;
 }
