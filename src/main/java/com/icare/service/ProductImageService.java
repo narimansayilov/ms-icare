26f1b4098b12b567a12369403275ea8df2bd651c
Nariman <narimansayilov@gmail.com>
2024-08-05 20:13:33 +0400

    add new method: editImages and getImages

--- src/main/java/com/icare/service/ProductImageService.java
+++ src/main/java/com/icare/service/ProductImageService.java
@@ -1,12 +1,16 @@
 package com.icare.service;
 
+import com.icare.dao.entity.ProductImageEntity;
 import com.icare.dao.repository.ProductImageRepository;
 import com.icare.mapper.ProductImageMapper;
 import com.icare.model.dto.request.ProductImageRequest;
+import com.icare.model.dto.response.ProductImageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -28,6 +32,23 @@ public class ProductImageService {
             main = !main;
         }
         log.info("ActionLog.addImages.end for product id = {}", productId);
+    }
+
+    public void editImages(MultipartFile[] images, Long productId) {
+        log.info("ActionLog.editImages.start for product id = {}", productId);
+        List<ProductImageEntity> productImages = productImageRepository.findByProductIdAndStatus(productId, true);
+        productImages.forEach(entity -> entity.setStatus(false));
+        productImageRepository.saveAll(productImages);
+        addImages(images, productId);
+        log.info("ActionLog.editImages.end for product id = {}", productId);
+    }
+
+    public List<ProductImageResponse> getImages(Long productId) {
+        log.info("ActionLog.getImages.start for product id = {}", productId);
+        List<ProductImageEntity> entities = productImageRepository.findByProductIdAndStatus(productId, true);
+        List<ProductImageResponse> responses = ProductImageMapper.INSTANCE.entitiesToResponses(entities);
+        log.info("ActionLog.getImages.end for product id = {}", productId);
+        return responses;
     }
 }
 
