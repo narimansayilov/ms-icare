92e7906c70836d109e36ce42ef7b85ae7d03551d
Nariman <narimansayilov@gmail.com>
2024-08-05 20:12:39 +0400

    add new method: findByProductIdAndStatus

--- src/main/java/com/icare/dao/repository/ProductImageRepository.java
+++ src/main/java/com/icare/dao/repository/ProductImageRepository.java
@@ -4,6 +4,9 @@ import com.icare.dao.entity.ProductImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImageEntity, Long> {
+    List<ProductImageEntity> findByProductIdAndStatus(Long productId, boolean status);
 }
