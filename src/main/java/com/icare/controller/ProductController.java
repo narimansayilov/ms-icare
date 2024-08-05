187354389c16f3760cacc4174e2fc9519b24c607
Nariman <narimansayilov@gmail.com>
2024-08-05 20:10:34 +0400

    add new endpoint: updateProduct

--- src/main/java/com/icare/controller/ProductController.java
+++ src/main/java/com/icare/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.icare.controller;
 
 import com.icare.model.dto.request.ProductRequest;
+import com.icare.model.dto.response.ProductResponse;
 import com.icare.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +20,43 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addProduct(@RequestPart("request") ProductRequest request,
                            @RequestPart("images") MultipartFile[] images){
-        productService.addProduct(request, images);
+        String email = getCurrentUsername();
+        productService.addProduct(request, images, email);
+    }
+
+    @GetMapping("/{id}/details")
+    public ProductResponse getProduct(@PathVariable Long id){
+        return productService.getProduct(id);
+    }
+
+    @PutMapping("/{id}/update")
+    public ProductResponse updateProduct(@PathVariable Long id,
+                                         @RequestPart("request") ProductRequest request,
+                                         @RequestPart("images") MultipartFile[] images){
+        String email = getCurrentUsername();
+        return productService.editById(id, request, images, email);
+    }
+
+    @PatchMapping("/{id}/activate")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void activateProduct(@PathVariable Long id){
+        String email = getCurrentUsername();
+        productService.activateProduct(id, email);
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deactivateProduct(@PathVariable Long id){
+        String email = getCurrentUsername();
+        productService.deactivateProduct(id, email);
+    }
+
+    private String getCurrentUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
     }
 }
-
