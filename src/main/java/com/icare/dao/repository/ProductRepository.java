711bc454a782d67e9b6f493ac383a80ed3b67bf8
Nariman <narimansayilov@gmail.com>
2024-08-11 00:35:02 +0400

    extend PagingAndSortingRepository and JpaSpecificationExecutor

--- src/main/java/com/icare/dao/repository/ProductRepository.java
+++ src/main/java/com/icare/dao/repository/ProductRepository.java
@@ -2,11 +2,10 @@ package com.icare.dao.repository;
 
 import com.icare.dao.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    List<ProductEntity> findByUserId(Long userId);
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, PagingAndSortingRepository<ProductEntity, Long>, JpaSpecificationExecutor<ProductEntity> {
 }
