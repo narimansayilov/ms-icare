74dfb92819a3bbbd93c98c56665cef5e553df209
Nariman <narimansayilov@gmail.com>
2024-08-11 00:59:34 +0400

    change validations

--- src/main/java/com/icare/model/dto/request/UserLoginRequest.java
+++ src/main/java/com/icare/model/dto/request/UserLoginRequest.java
@@ -1,9 +1,7 @@
 package com.icare.model.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import com.icare.util.annotation.ValidEmail;
+import com.icare.util.annotation.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,15 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequest {
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email cannot be blank")
+    @ValidEmail
     private String email;
 
-    @Size(min = 8, max = 64, message = "Password must be at least 8 characters long")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=?!.,]).{8,}$",
-            message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character"
-    )
-    @NotBlank(message = "Password cannot be blank")
+    @ValidPassword
     private String password;
 }
