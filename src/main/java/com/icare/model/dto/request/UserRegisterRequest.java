3827e86e53f90a9f4be65ca11c26688070ed4784
Nariman <narimansayilov@gmail.com>
2024-08-11 00:59:46 +0400

    change validations

--- src/main/java/com/icare/model/dto/request/UserRegisterRequest.java
+++ src/main/java/com/icare/model/dto/request/UserRegisterRequest.java
@@ -1,5 +1,7 @@
 package com.icare.model.dto.request;
 
+import com.icare.util.annotation.ValidEmail;
+import com.icare.util.annotation.ValidPassword;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +11,12 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UserRegisterRequest {
+    @ValidEmail
+    private String email;
+
+    @ValidPassword
+    private String password;
+
     @Size(min = 7, max = 7, message = "Pin must be exactly 7 characters long")
     @Pattern(regexp = "^[A-Z0-9]+$", message = "Pin must contain only English letters and numbers")
     @NotBlank(message = "Pin cannot be blank")
@@ -27,18 +35,6 @@ public class UserRegisterRequest {
     @Past(message = "Birth day must be a past date")
     @NotNull(message = "Birth day cannot be blank")
     private LocalDate birthDay;
-
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email cannot be blank")
-    private String email;
-
-    @Size(min = 8, max = 64, message = "Password must be at least 8 characters long")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=?!.,]).{8,}$",
-            message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character"
-    )
-    @NotBlank(message = "Password cannot be blank")
-    private String password;
 
     @Pattern(regexp = "^\\+994\\d{9}$", message = "Phone number must be in the format +994XXXXXXXXX")
     @NotBlank(message = "Phone number cannot be blank")
