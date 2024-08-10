d844d5763f5600a0d06df17629e0a0cc21f543c6
Nariman <narimansayilov@gmail.com>
2024-08-11 00:35:20 +0400

    add new validation

--- src/main/java/com/icare/model/dto/request/ProductRequest.java
+++ src/main/java/com/icare/model/dto/request/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.icare.model.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +9,30 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ProductRequest {
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min = 3, max = 255, message = "Title must be between 3 and 100 characters")
     private String title;
+
+    @Size(max = 2000, message = "Description must be up to 2000 characters")
     private String description;
+
+    @NotNull(message = "Price per day cannot be null")
+    @DecimalMin(value = "0.1", message = "Price per day must be greater than zero")
     private Double pricePerDay;
+
+    @NotBlank(message = "Address cannot be blank")
+    @Size(min = 3, max = 511, message = "Address must be between 3 and 511 characters")
     private String address;
-    private boolean deliveryAvailable;
+
+    @NotNull(message = "Delivery availability cannot be null")
+    private Boolean deliveryAvailable;
     private Double deliveryPricePerKm;
+
+    @NotNull(message = "Category ID cannot be null")
+    @Positive(message = "Category ID must be a positive number")
     private Long categoryId;
+
+    @NotNull(message = "City ID cannot be null")
+    @Positive(message = "Cİty ID must be a positive number")
     private Long cityId;
 }
