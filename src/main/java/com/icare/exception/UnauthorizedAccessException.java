07cad867ad28c150d04c474b0ba10d9b6a860ae0
Nariman <narimansayilov@gmail.com>
2024-08-13 23:49:45 +0400

    add new unauthorized exception

--- src/main/java/com/icare/exception/UnauthorizedAccessException.java
+++ src/main/java/com/icare/exception/UnauthorizedAccessException.java
@@ -1,4 +1,4 @@
-package com.icare.model.exception;
+package com.icare.exception;
 
 public class UnauthorizedAccessException extends RuntimeException{
     public UnauthorizedAccessException(String message){
