c285dd1fcc6af69369d0b6d058b1463929664b66
Nariman <narimansayilov@gmail.com>
2024-08-20 14:20:50 +0400

    add new annotation

--- src/main/java/com/icare/MsIcareApplication.java
+++ src/main/java/com/icare/MsIcareApplication.java
@@ -2,10 +2,11 @@ package com.icare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsIcareApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsIcareApplication.class, args);
     }
