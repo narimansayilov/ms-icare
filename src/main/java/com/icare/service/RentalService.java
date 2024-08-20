54f47f19cee3a706964232183bb9d19197f0caeb
Nariman <narimansayilov@gmail.com>
2024-08-20 14:08:57 +0400

    add new service

---
+++ src/main/java/com/icare/service/RentalService.java
@@ -0,0 +1,67 @@
+package com.icare.service;
+
+import com.icare.dto.request.DeliveryRequest;
+import com.icare.dto.request.RentalRequest;
+import com.icare.dto.response.DeliveryDetailsResponse;
+import com.icare.entity.ProductEntity;
+import com.icare.entity.RentalEntity;
+import com.icare.enums.DeliveryMethod;
+import com.icare.enums.RentalStatus;
+import com.icare.exception.NotFoundException;
+import com.icare.mapper.RentalMapper;
+import com.icare.repository.ProductRepository;
+import com.icare.repository.RentalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RentalService {
+    private final RentalRepository rentalRepository;
+    private final DeliveryService deliveryService;
+    private final ProductRepository productRepository;
+
+    public double addRental(RentalRequest request, Long orderId) {
+        double totalPrice = 0.0;
+        ProductEntity product = productRepository.findById(request.getProductId()).orElseThrow(() ->
+                new NotFoundException("PRODUCT_NOT_FOUND"));
+
+        long rentalDays = ChronoUnit.DAYS.between(request.getRentalStartDate(), request.getRentalEndDate());
+        double rentalCost = product.getPricePerDay() * rentalDays;
+        totalPrice += rentalCost;
+
+        RentalEntity entity = RentalMapper.INSTANCE.requestToEntity(request, orderId, RentalStatus.ORDER_PLACED);
+
+        double deliveryCost = calculateDeliveryCost(request.getDeliveryMethod(), request.getDeliveryAddress(), request.getProductId(), true);
+        entity.setDeliveryCost(deliveryCost);
+        totalPrice += deliveryCost;
+
+        double returnedDeliveryCost = calculateDeliveryCost(request.getReturnedDeliveryMethod(), request.getDeliveryAddress(), request.getProductId(), false);
+        entity.setReturnedDeliveryCost(returnedDeliveryCost);
+        totalPrice += returnedDeliveryCost;
+
+        entity.setRentalConst(rentalCost);
+        rentalRepository.save(entity);
+        return totalPrice;
+    }
+
+    private double calculateDeliveryCost(DeliveryMethod method, String address, Long productId, boolean isDelivery) {
+        if (method == DeliveryMethod.PICKUP) {
+            return DeliveryMethod.PICKUP.getPrice();
+        } else {
+            DeliveryRequest deliveryRequest = DeliveryRequest.builder()
+                    .address(address)
+                    .productId(productId)
+                    .method(method)
+                    .build();
+
+            DeliveryDetailsResponse deliveryResponse = isDelivery ?
+                    deliveryService.getDelivery(deliveryRequest) :
+                    deliveryService.getReturnedDelivery(deliveryRequest);
+
+            return deliveryResponse.getAmount();
+        }
+    }
+}
