0bc1ae45074ae2a165fab57e2811cf47aff098ba
Nariman <narimansayilov@gmail.com>
2024-08-20 14:07:37 +0400

    add new controller

---
+++ src/main/java/com/icare/controller/DeliveryController.java
@@ -0,0 +1,24 @@
+package com.icare.controller;
+
+import com.icare.dto.request.DeliveryRequest;
+import com.icare.dto.response.DeliveryDetailsResponse;
+import com.icare.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/distance")
+@RequiredArgsConstructor
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    @PostMapping
+    public DeliveryDetailsResponse calculateDelivery(@RequestBody DeliveryRequest request) {
+        return deliveryService.getDelivery(request);
+    }
+
+    @PostMapping("/returned")
+    public DeliveryDetailsResponse calculateReturnedDelivery(@RequestBody DeliveryRequest request) {
+        return deliveryService.getReturnedDelivery(request);
+    }
+}
