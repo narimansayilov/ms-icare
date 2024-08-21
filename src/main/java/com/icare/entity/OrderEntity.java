2637e008d7da7bec8950139574bb6f47c4723e65
Nariman <narimansayilov@gmail.com>
2024-08-21 23:51:17 +0400

    change entity structure

--- src/main/java/com/icare/entity/OrderEntity.java
+++ src/main/java/com/icare/entity/OrderEntity.java
@@ -1,6 +1,5 @@
 package com.icare.entity;
 
-import com.icare.enums.DeliveryMethod;
 import com.icare.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.*;
@@ -9,6 +8,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+
+import static com.icare.enums.OrderStatus.PENDING;
 
 @Entity
 @Getter
@@ -35,4 +36,9 @@ public class OrderEntity {
 
     @UpdateTimestamp
     LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void autoFill(){
+        this.status = PENDING;
+    }
 }
