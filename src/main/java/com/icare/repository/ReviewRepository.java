eaacbece8b1f08adaecf039e475a748438d411b4
Nariman <narimansayilov@gmail.com>
2024-08-21 23:52:51 +0400

    add new method

--- src/main/java/com/icare/repository/ReviewRepository.java
+++ src/main/java/com/icare/repository/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.icare.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+    List<ReviewEntity> findAllByProductId(Long productId);
 }
