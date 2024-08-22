c1e73e740cf877848074c0d7e542217014545e13
Nariman <narimansayilov@gmail.com>
2024-08-22 23:54:29 +0400

    add new endpoint

--- src/main/java/com/icare/controller/ReviewController.java
+++ src/main/java/com/icare/controller/ReviewController.java
@@ -1,10 +1,14 @@
 package com.icare.controller;
 
 import com.icare.dto.request.ReviewRequest;
+import com.icare.dto.response.ReviewResponse;
 import com.icare.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +20,20 @@ public class ReviewController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addReview(@RequestBody ReviewRequest request){
         reviewService.addReview(request);
+    }
+
+    @GetMapping("/{id}")
+    public List<ReviewResponse> getProductReviews(@PathVariable Long id, Pageable pageable){
+        return reviewService.getProductReviews(id, pageable);
+    }
+
+    @PutMapping("/{id}")
+    public ReviewResponse updateReview(@PathVariable Long id, @RequestBody ReviewRequest request){
+        return reviewService.updateReview(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Long id){
+        reviewService.deleteReview(id);
     }
 }
