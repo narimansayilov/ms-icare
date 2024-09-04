fe486cf325a724954d7ba57aaba5509b0711a53f
Nariman <narimansayilov@gmail.com>
2024-09-04 16:37:18 +0400

    add new controller

---
+++ src/main/java/com/icare/controller/RoleController.java
@@ -0,0 +1,43 @@
+package com.icare.controller;
+
+import com.icare.dto.request.RoleRequest;
+import com.icare.dto.response.RoleResponse;
+import com.icare.service.RoleService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+public class RoleController {
+    private final RoleService roleService;
+
+    @PostMapping
+    public void addRole(@RequestBody @Valid RoleRequest request) {
+        roleService.addRole(request);
+    }
+
+    @GetMapping("/{id}")
+    public RoleResponse getRole(@PathVariable Long id) {
+        return roleService.getRole(id);
+    }
+
+    @GetMapping
+    public List<RoleResponse> getAllRoles() {
+        return roleService.getAllRoles();
+    }
+
+    @PutMapping("/{id}")
+    public RoleResponse updateRole(@PathVariable Long id,
+                                   @RequestBody @Valid RoleRequest request) {
+        return roleService.updateRole(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRole(@PathVariable Long id) {
+        roleService.deleteRole(id);
+    }
+}
