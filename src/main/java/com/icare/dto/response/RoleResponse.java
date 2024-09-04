1de0fd95dd5bac42ae9b6653e054bd9ed6dd43f1
Nariman <narimansayilov@gmail.com>
2024-09-04 16:37:43 +0400

    add new dto

---
+++ src/main/java/com/icare/dto/response/RoleResponse.java
@@ -0,0 +1,13 @@
+package com.icare.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class RoleResponse {
+    private Long id;
+    private String name;
+}
