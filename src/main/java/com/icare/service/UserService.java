ded224c9ab86f6b014586c0f2361e856749cba95
Nariman <narimansayilov@gmail.com>
2024-09-04 16:35:17 +0400

    add new method

--- src/main/java/com/icare/service/UserService.java
+++ src/main/java/com/icare/service/UserService.java
@@ -1,28 +1,25 @@
 package com.icare.service;
 
-import com.icare.entity.LevelEntity;
+import com.icare.dto.criteria.UserCriteriaRequest;
+import com.icare.dto.request.UserUpdateRequest;
+import com.icare.dto.response.UserResponse;
 import com.icare.entity.RoleEntity;
 import com.icare.entity.UserEntity;
-import com.icare.repository.LevelRepository;
+import com.icare.exception.ActiveException;
+import com.icare.exception.AlreadyExistsException;
+import com.icare.exception.NotActiveException;
+import com.icare.exception.NotFoundException;
+import com.icare.mapper.UserMapper;
 import com.icare.repository.RoleRepository;
 import com.icare.repository.UserRepository;
-import com.icare.mapper.UserMapper;
-import com.icare.dto.request.UserLoginRequest;
-import com.icare.dto.request.UserRegisterRequest;
-import com.icare.dto.request.UserUpdateRequest;
-import com.icare.dto.response.JwtResponse;
-import com.icare.dto.response.UserResponse;
-import com.icare.exception.AlreadyExistsException;
-import com.icare.exception.NotFoundException;
+import com.icare.service.specification.UserSpecification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,35 +30,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final JwtService jwtService;
     private final AmazonS3Service amazonS3Service;
-    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
-    private final LevelRepository levelRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-
-    @Transactional
-    public void register(UserRegisterRequest request) {
-        userRepository.findByEmail(request.getEmail()).ifPresent(user -> {
-            throw new AlreadyExistsException("EMAIL_ALREADY_EXISTS");
-        });
-        UserEntity userEntity = UserMapper.INSTANCE.registerRequestToEntity(request);
-        userEntity.setPassword(passwordEncoder.encode(request.getPassword()));
-        userEntity.setRoles(List.of(getRole()));
-        userEntity.setLevel(getLevel());
-        userRepository.save(userEntity);
-    }
-
-    public JwtResponse login(UserLoginRequest request) {
-        userRepository.findByEmail(request.getEmail()).orElseThrow(() ->
-                new NotFoundException("EMAIL_NOT_FOUND"));
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        User principal = (User) authentication.getPrincipal();
-        String accessToken = jwtService.generateAccessToken(principal);
-        return new JwtResponse(principal.getUsername(), accessToken);
-    }
+    public final RoleRepository roleRepository;
 
     public UserResponse getUser(Long id) {
         UserEntity entity = userRepository.findById(id).orElseThrow(() ->
@@ -74,6 +45,12 @@ public class UserService {
         UserEntity entity = userRepository.findByEmail(email).orElseThrow(() ->
                 new NotFoundException("USER_NOT_FOUND"));
         return UserMapper.INSTANCE.entityToResponse(entity);
+    }
+
+    public List<UserResponse> getAllUsers(Pageable pageable, UserCriteriaRequest criteriaRequest){
+        Specification<UserEntity> specification = UserSpecification.getUserByCriteria(criteriaRequest);
+        Page<UserEntity> entities = userRepository.findAll(specification, pageable);
+        return UserMapper.INSTANCE.entitiesToResponses(entities);
     }
 
     @Transactional
@@ -92,6 +69,35 @@ public class UserService {
         return UserMapper.INSTANCE.entityToResponse(entity);
     }
 
+    public void activateUser(Long id){
+        UserEntity entity = userRepository.findById(id).orElseThrow(() ->
+                new NotFoundException("USER_NOT_FOUND"));
+        if(entity.getStatus()){
+            throw new ActiveException("USER_ALREADY_ACTIVE");
+        }
+        entity.setStatus(true);
+        userRepository.save(entity);
+    }
+
+    public void deactivateUser(Long id){
+        UserEntity entity = userRepository.findById(id).orElseThrow(() ->
+                new NotFoundException("USER_NOT_FOUND"));
+        if(!entity.getStatus()){
+            throw new NotActiveException("USER_NOT_ACTIVE");
+        }
+        entity.setStatus(false);
+        userRepository.save(entity);
+    }
+
+    public void setRole(Long userId, Long roleId){
+        UserEntity user = userRepository.findById(userId).orElseThrow(() ->
+                new NotFoundException("USER_NOT_FOUND"));
+        RoleEntity role = roleRepository.findById(roleId).orElseThrow(() ->
+                new NotFoundException("ROLE_NOT_FOUND"));
+        user.setRoles(List.of(role));
+        userRepository.save(user);
+    }
+
     public String getCurrentEmail(){
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
@@ -99,24 +105,5 @@ public class UserService {
         } else {
             return principal.toString();
         }
-    }
-
-    private RoleEntity getRole() {
-        return roleRepository.findByName("USER")
-                .orElseGet(() -> roleRepository.save(RoleEntity.builder()
-                        .name("USER")
-                        .build()));
-    }
-
-    private LevelEntity getLevel() {
-        LevelEntity level = levelRepository.findByName("Bronze")
-                .orElseGet(() -> levelRepository.save(LevelEntity.builder()
-                        .name("Bronze")
-                        .adLimit(10)
-                        .price(0.0)
-                        .userCount(0)
-                        .build()));
-        level.setUserCount(level.getUserCount() + 1);
-        return level;
     }
 }
