07914c7410f2f06f20e73dacb9a558dbb714748d
Nariman <narimansayilov@gmail.com>
2024-09-04 16:35:01 +0400

    add new method

--- src/main/java/com/icare/repository/UserRepository.java
+++ src/main/java/com/icare/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.icare.repository;
 
 import com.icare.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long>, PagingAndSortingRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
     Optional<UserEntity> findByEmail(String email);
     Boolean existsByEmail(String email);
 }
