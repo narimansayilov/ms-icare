baf410100ff4760a713d8584394e9c5f25d7e1d5
Nariman <narimansayilov@gmail.com>
2024-09-04 16:33:02 +0400

    add new endpoint

--- src/main/java/com/icare/controller/UserController.java
+++ src/main/java/com/icare/controller/UserController.java
@@ -1,33 +1,22 @@
 package com.icare.controller;
 
-import com.icare.dto.request.UserLoginRequest;
-import com.icare.dto.request.UserRegisterRequest;
+import com.icare.dto.criteria.UserCriteriaRequest;
 import com.icare.dto.request.UserUpdateRequest;
-import com.icare.dto.response.JwtResponse;
 import com.icare.dto.response.UserResponse;
 import com.icare.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("users")
 public class UserController {
     private final UserService userService;
-
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void register(@RequestBody @Valid UserRegisterRequest request) {
-        userService.register(request);
-    }
-
-    @PostMapping("/login")
-    public JwtResponse login(@RequestBody @Valid UserLoginRequest request) {
-        return userService.login(request);
-    }
 
     @GetMapping("/my")
     public UserResponse getMyProfile(){
@@ -39,9 +28,30 @@ public class UserController {
         return userService.getUser(id);
     }
 
+    @GetMapping("/all")
+    public List<UserResponse> getAllUsers(Pageable pageable, UserCriteriaRequest criteriaRequest) {
+        return userService.getAllUsers(pageable, criteriaRequest);
+    }
+
     @PutMapping("/update")
     public UserResponse update(@RequestPart("request") @Valid UserUpdateRequest request,
                                @RequestPart("image") MultipartFile image) {
         return userService.update(request, image);
+    }
+
+    @PatchMapping("/{id}/activate")
+    public void activateUser(@PathVariable Long id) {
+        userService.activateUser(id);
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public void deactivateUser(@PathVariable Long id) {
+        userService.deactivateUser(id);
+    }
+
+    @PatchMapping("/set-role")
+    public void setRole(@RequestParam(name = "user") Long userId,
+                        @RequestParam(name = "role") Long roleId) {
+        userService.setRole(userId, roleId);
     }
 }
