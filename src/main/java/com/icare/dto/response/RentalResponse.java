7afcab3033071a8f9bf1b2c80eb78fbe247f484e
Nariman <narimansayilov@gmail.com>
2024-09-19 10:41:26 +0400

    add new dto

--- src/main/java/com/icare/dto/response/RentalResponse.java
+++ src/main/java/com/icare/dto/response/RentalResponse.java
@@ -1,9 +1,24 @@
 package com.icare.dto.response;
 
+import com.icare.enums.DeliveryMethod;
+import com.icare.enums.RentalStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class RentalResponse {
+    private Long id;
+    private LocalDate rentalStartDate;
+    private LocalDate rentalEndDate;
+    private Double rentalCost;
+    private String deliveryAddress;
+    private DeliveryMethod deliveryMethod;
+    private Double deliveryCost;
+    private DeliveryMethod returnedDeliveryMethod;
+    private Double returnedDeliveryCost;
+    private RentalStatus status;
+    private ProductResponse product;
 }
--- src/main/java/com/icare/mapper/RentalMapper.java
+++ src/main/java/com/icare/mapper/RentalMapper.java
@@ -1,12 +1,15 @@
 package com.icare.mapper;
 
 import com.icare.dto.request.RentalRequest;
+import com.icare.dto.response.RentalResponse;
 import com.icare.entity.RentalEntity;
 import com.icare.enums.RentalStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface RentalMapper {
@@ -15,4 +18,8 @@ public interface RentalMapper {
     @Mapping(source = "orderId", target = "order.id")
     @Mapping(source = "request.productId", target = "product.id")
     RentalEntity requestToEntity(RentalRequest request, Long orderId);
+
+    RentalResponse entityToResponse(RentalEntity entity);
+
+    List<RentalResponse> entitiesToResponses(List<RentalEntity> entities);
 }
