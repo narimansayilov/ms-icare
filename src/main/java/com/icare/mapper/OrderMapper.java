ca9a1fd94a1859db9d99b3ba333014a30b7e217c
Nariman <narimansayilov@gmail.com>
2024-09-19 10:40:52 +0400

    add new mapper

--- src/main/java/com/icare/mapper/OrderMapper.java
+++ src/main/java/com/icare/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.icare.mapper;
 
+import com.icare.dto.response.OrderResponse;
+import com.icare.dto.response.RentalResponse;
 import com.icare.entity.OrderEntity;
 import com.icare.entity.UserEntity;
 import org.mapstruct.Mapper;
@@ -7,10 +9,16 @@ import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     @Mapping(source = "userId", target = "user.id")
     OrderEntity requestToEntity(Long userId);
+
+    OrderResponse entityToResponse(OrderEntity orderEntity, List<RentalResponse> rentals);
+
+    List<OrderResponse> entitiesToResponses(List<OrderEntity> entities);
 }
