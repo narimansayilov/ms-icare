e4c962da956b2edcfc79837ff5966e9e4f2e94fc
Nariman <narimansayilov@gmail.com>
2024-09-19 10:41:17 +0400

    add new dto

--- src/main/java/com/icare/dto/response/OrderResponse.java
+++ src/main/java/com/icare/dto/response/OrderResponse.java
@@ -1,9 +1,16 @@
 package com.icare.dto.response;
 
+import com.icare.enums.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class OrderResponse {
+    private Long id;
+    private Double amount;
+    private OrderStatus status;
+    private List<RentalResponse> rentals;
 }
